04f1ba58b096d8b8b9a4a72bee4b4fce8e898acf
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2025-05-24 16:52:07 +0300

    init commit

---
+++ src/main/java/com/hacisimsek/ecommerce_microservices/EcommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.hacisimsek.ecommerce_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacisimsek/ecommerce_microservices/EcommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacisimsek.ecommerce_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
