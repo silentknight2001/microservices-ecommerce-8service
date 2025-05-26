cbb527845e536d887baf3f4c7fe12597491257e1
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2025-05-26 21:26:45 +0300

    Add auditing support to inventory items with MongoDB

--- inventory-service/src/main/java/com/hacisimsek/inventory/model/InventoryItem.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/model/InventoryItem.java
@@ -1,6 +1,9 @@
 package com.hacisimsek.inventory.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -8,6 +11,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document(collection = "inventory")
@@ -22,4 +26,20 @@ public class InventoryItem {
     private String description;
     private Integer availableQuantity;
     private Integer reservedQuantity;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    // Manually setting these since we're not using MongoAuditing in this example
+    @Builder.Default
+    private LocalDateTime creationTimestamp = LocalDateTime.parse("2025-05-26T17:49:38");
+
+    @Builder.Default
+    private String creator = "simsekhacitekrar";
 }
