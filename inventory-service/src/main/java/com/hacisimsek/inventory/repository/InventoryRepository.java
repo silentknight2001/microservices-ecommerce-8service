ec1f5592f6aa1d49d0c84e58e7f28220cd5d4346
silentknight2001 <silentknight.www@gmail.com>
2026-06-03 12:42:22 +0530

    fix: UUID change to String

--- inventory-service/src/main/java/com/hacisimsek/inventory/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/repository/InventoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.UUID;
 
-public interface InventoryRepository extends MongoRepository<InventoryItem, UUID> {
+public interface InventoryRepository extends MongoRepository<InventoryItem, String> {
 }
