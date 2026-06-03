3dfb0a5c36cab249617f81c155a2b0e60db1bd40
silentknight2001 <silentknight.www@gmail.com>
2026-06-03 12:35:38 +0530

    fix:error resolved is was created UUID inted of String because mongodb comfortable with String not UUID, for postgresql UUID is good fit.

--- inventory-service/src/main/java/com/hacisimsek/inventory/repository/ReservationRepository.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/repository/ReservationRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ReservationRepository extends MongoRepository<InventoryReservation, UUID> {
-    Optional<InventoryReservation> findByOrderId(UUID orderId);
+public interface ReservationRepository extends MongoRepository<InventoryReservation, String> {
+    Optional<InventoryReservation> findByOrderId(String orderId);
 }
