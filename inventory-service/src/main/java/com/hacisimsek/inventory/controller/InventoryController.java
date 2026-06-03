c861b41371892800b0a97f2a1a1c5e77d32c986a
silentknight2001 <silentknight.www@gmail.com>
2026-06-03 13:26:47 +0530

    test : pipeline

--- inventory-service/src/main/java/com/hacisimsek/inventory/controller/InventoryController.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/controller/InventoryController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
+// import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
