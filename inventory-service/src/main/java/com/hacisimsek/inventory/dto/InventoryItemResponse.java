156940b2e171748a0d5fc81416354098a0079089
silentknight2001 <silentknight.www@gmail.com>
2026-08-10 01:00:08 +0530

    add:missing productId in impl file

--- inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemResponse.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemResponse.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class InventoryItemResponse {
     private String id;
+    private String productId;
     private String name;
     private String description;
     private Integer availableQuantity;
