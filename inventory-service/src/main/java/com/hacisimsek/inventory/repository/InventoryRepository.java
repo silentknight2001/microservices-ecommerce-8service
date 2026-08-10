1f85f74cf79b01789caa4faec8ac104bb856c753
silentknight2001 <silentknight.www@gmail.com>
2026-08-10 17:01:18 +0530

    fix:wrong query fixed for mongodb in repoitory file

--- inventory-service/src/main/java/com/hacisimsek/inventory/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/repository/InventoryRepository.java
@@ -2,8 +2,8 @@ package com.hacisimsek.inventory.repository;
 
 import com.hacisimsek.inventory.model.InventoryItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
+import java.util.Optional;
 
 public interface InventoryRepository extends MongoRepository<InventoryItem, String> {
+    Optional<InventoryItem> findByProductId(String productId);
 }
