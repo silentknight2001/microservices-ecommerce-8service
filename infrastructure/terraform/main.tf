be77d788cdb438deae95fae365ad27b4296a83f2
silentknight2001 <silentknight.www@gmail.com>
2026-08-26 13:34:00 +0530

    test:test workflow for terrafrom infra

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   availability_zones   = var.availability_zones
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  cluster_name         = var.cluster_name
+  cluster_name         = var.cluster_name              #test run workflow
 
 }
 
