908a3c4ed7da4665a0b1c861cccab3f637022b07
silentknight2001 <silentknight.www@gmail.com>
2026-09-01 14:07:40 +0530

    fix:eks fille

--- infrastructure/terraform/modules/eks/main.tf
+++ infrastructure/terraform/modules/eks/main.tf
@@ -144,6 +144,12 @@ resource "aws_eks_addon" "ebs_csi" {
   ]
 }
 
+# modules/eks/main.tf — add to node role
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_eks_addon" "pod_identity" {
   cluster_name                = aws_eks_cluster.main.name
   addon_name                  = "eks-pod-identity-agent"
