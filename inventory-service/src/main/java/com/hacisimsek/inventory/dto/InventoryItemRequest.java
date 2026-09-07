49002a0005bd95e4f284bb09c9935e03bc5aad15
silentknight2001 <silentknight.www@gmail.com>
2026-09-07 23:53:48 +0530

    fix: use double quotes in sed for SHORT_SHA expansion

--- inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemRequest.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;  
+import lombok.NoArgsConstructor;  //adding dummy comment for test ci
 
 @Data
 @NoArgsConstructor
