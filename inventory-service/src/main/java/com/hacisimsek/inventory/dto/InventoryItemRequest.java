459907eaec7fd4829af1c3d29a8039c03c7d0a5c
silentknight2001 <silentknight.www@gmail.com>
2026-09-07 23:01:39 +0530

    ci: triger ci for test argocd

--- inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemRequest.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;  
+import lombok.NoArgsConstructor;  // dummy commnet for trigger cd test using argocd 
 
 @Data
 @NoArgsConstructor
