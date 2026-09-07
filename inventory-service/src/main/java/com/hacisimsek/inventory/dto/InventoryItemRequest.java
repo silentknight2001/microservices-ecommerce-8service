30569481b73c0b61c343259f6b2ffcbcc3de40f9
silentknight2001 <silentknight.www@gmail.com>
2026-09-07 23:39:10 +0530

    ci: triger ci inventory

--- inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemRequest.java
+++ inventory-service/src/main/java/com/hacisimsek/inventory/dto/InventoryItemRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;  
+import lombok.NoArgsConstructor;   // dummy commenty for test ci 
 
 @Data
 @NoArgsConstructor
