86ce301e10fb80c1be2c505b3fbbaad7ce392566
silentknight2001 <silentknight.www@gmail.com>
2026-09-15 02:01:37 +0530

    fix wrong path

--- order-service/src/main/java/com/hacisimsek/order/config/KafkaConfig.java
+++ order-service/src/main/java/com/hacisimsek/order/config/KafkaConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;    //test ci
+import org.springframework.kafka.support.serializer.JsonSerializer;    
 
 import java.util.HashMap;
 import java.util.Map;
